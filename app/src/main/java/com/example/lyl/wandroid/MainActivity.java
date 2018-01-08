54d9a1fb23295cef7b91c847e58005eec5f5e36a
louyulin <1245337273@qq.com>
2018-01-08 18:03:27 +0800

    5

--- app/src/main/java/com/example/lyl/wandroid/MainActivity.java
+++ app/src/main/java/com/example/lyl/wandroid/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   //555dsafa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
     }
 }
